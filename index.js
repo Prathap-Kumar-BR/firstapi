e533690c613ecb53d4ae00fd9bcdca4509835382
prathapbr1993 <82834371+prathapbr1993@users.noreply.github.com>
2021-11-26 20:25:01 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+var cors = require("cors");
 const dbConnect = require("./server/config/db");
 const firstapiRoute = require("./server/routes/firstapiRoutes");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/", firstapiRoute);
 
